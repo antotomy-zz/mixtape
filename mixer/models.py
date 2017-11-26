1ed043228198e17a94ed215b1a344a844a456c9e
Anto P T <anto.pt@instamojo.com>
2017-11-26 14:41:05 +0530

    Make Song model part of Mixtape

--- mixer/models.py
+++ mixer/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class Song(models.Model):
+	mixtape = models.ForeignKey('Mixtape', related_name='songs')
+	url = models.URLField()
 	name = models.CharField(max_length=255)
 	artist_name = models.CharField(max_length=255)
 	position = models.PositiveSmallIntegerField()
@@ -16,10 +18,10 @@ class Mixtape(models.Model):
 		settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
 		related_name='mixtapes')
 	name = models.CharField(max_length=255)
-	played_count = models.PositiveIntegerField()
-	songs = models.ManyToManyField(Song, related_name='+')
+	played_count = models.PositiveIntegerField(default=0)
 	saved_users = models.ManyToManyField(
-		settings.AUTH_USER_MODEL, related_name='saved_mixtapes')
+		settings.AUTH_USER_MODEL, blank=True,
+		related_name='saved_mixtapes')
 
 	def __str__(self):
 		return self.name
